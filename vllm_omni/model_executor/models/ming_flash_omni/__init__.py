22b2f3056839f4b7dc87cbb437a08ae47abd358f
yuanheng <jonathan.zhaoyh@gmail.com>
2026-02-24 18:11:53 +0800

    draft: add processor
    Signed-off-by: yuanheng <jonathan.zhaoyh@gmail.com>

--- vllm_omni/model_executor/models/ming_flash_omni/__init__.py
+++ vllm_omni/model_executor/models/ming_flash_omni/__init__.py
@@ -2,9 +2,19 @@
 # Copyright 2025 The vLLM-Omni team.
 
 from .ming_flash_omni import MingFlashOmniForConditionalGeneration
-from .ming_flash_omni_thinker import MingFlashOmniThinkerForConditionalGeneration
+from .ming_flash_omni_thinker import (
+    MingFlashOmniThinkerDummyInputsBuilder,
+    MingFlashOmniThinkerForConditionalGeneration,
+    MingFlashOmniThinkerMultiModalProcessor,
+    MingFlashOmniThinkerProcessingInfo,
+)
+from .processing_ming_flash_omni import MingFlashOmniProcessor
 
 __all__ = [
     "MingFlashOmniForConditionalGeneration",
     "MingFlashOmniThinkerForConditionalGeneration",
+    "MingFlashOmniThinkerProcessingInfo",
+    "MingFlashOmniThinkerMultiModalProcessor",
+    "MingFlashOmniThinkerDummyInputsBuilder",
+    "MingFlashOmniProcessor",
 ]
