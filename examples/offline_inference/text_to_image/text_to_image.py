82e56ea99d952849550767f9164946fac4365067
yuanheng <jonathan.zhaoyh@gmail.com>
2026-04-06 17:44:10 +0800

    add init-timeout to t2i script
    Signed-off-by: yuanheng <jonathan.zhaoyh@gmail.com>

--- examples/offline_inference/text_to_image/text_to_image.py
+++ examples/offline_inference/text_to_image/text_to_image.py
@@ -243,6 +243,12 @@ def parse_args() -> argparse.Namespace:
         help="Enable logging of diffusion pipeline stats.",
     )
     parser.add_argument(
+        "--init-timeout",
+        type=int,
+        default=600,
+        help="Timeout for initializing a single stage in seconds (default: 600s)",
+    )
+    parser.add_argument(
         "--stage-init-timeout",
         type=int,
         default=600,
@@ -339,6 +345,7 @@ def main():
         "mode": "text-to-image",
         "log_stats": args.log_stats,
         "enable_diffusion_pipeline_profiler": args.enable_diffusion_pipeline_profiler,
+        "init_timeout": args.init_timeout,
         "stage_init_timeout": args.stage_init_timeout,
         **lora_args,
         **quant_kwargs,
